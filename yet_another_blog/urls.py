b96f4bfbeb2decd7b339a66e046af2c6acad98bb
Lucio Martínez <luciomartinez@openmailbox.org>
2014-11-24 23:01:09 -0300

    Added a harcoded index view for blog

--- yet_another_blog/urls.py
+++ yet_another_blog/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'yet_another_blog.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
     url('^markdown/', include('django_markdown.urls')),
