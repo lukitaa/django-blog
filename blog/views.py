085323d585cc365e8bae124cf9c360705314e12a
Lucio Martínez <luciomartinez@openmailbox.org>
2014-11-24 23:20:55 -0300

    Implemented index template using ugly HttpResponse and RequestContext methods

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext, loader
+
+from blog.models import Article
 
 
 def index(request):
-    return HttpResponse('Hello, World!')
+    latest_article_list = Article.objects.order_by('-created')[:3]
+    template = loader.get_template('blog/index.html')
+    context = RequestContext(request, {
+        'latest_article_list': latest_article_list,
+    })
+    return HttpResponse(template.render(context))
